4d90f44ef174896c942e800f93fb17500ce77bcb
JuanDG7 <142144090+JuanDG7@users.noreply.github.com>
2026-06-18 16:14:58 -0300

    hook created for statistic calculatios

--- frontend/src/components/CategoryStatCard.tsx
+++ frontend/src/components/CategoryStatCard.tsx
@@ -14,22 +14,21 @@ export function CategoryStatCard({
   countByCategory,
 }: CategoryStatCardProps) {
   return (
-    <>
-      {" "}
-      <div className="mb-4">
-        <p>
-          {category}: {formatGuarani(amount)} ({percentage.toFixed(1)}%) -{" "}
-          {countByCategory} gastos
-        </p>
-        <div className="h-6 w-full rounded bg-gray-200">
-          <div
-            className="h-6 rounded bg-black"
-            style={{
-              width: `${percentage}%`,
-            }}
-          />
-        </div>
+    <div className="mb-4">
+      <h3>{category}</h3>
+      <p>
+        {formatGuarani(amount)} • {countByCategory} gastos
+      </p>
+      <div className="h-6 w-full rounded bg-gray-200">
+        <div
+          className="h-6 rounded bg-black"
+          style={{
+            width: `${percentage}%`,
+          }}
+        />
       </div>
-    </>
+
+      <p>({percentage.toFixed(1)}%)</p>
+    </div>
   );
 }
---
+++ frontend/src/hooks/useExpenseCalculations.ts
@@ -0,0 +1,100 @@
+import type { Expense } from "../types/expense";
+interface UseExpenseCalculationsProps {
+  expenses: Expense[];
+  selectedCategory: string;
+  searchTerm: string;
+  tagSearch: string;
+  sort: string;
+}
+
+export function useExpenseCalculations({
+  expenses,
+  selectedCategory,
+  searchTerm,
+  tagSearch,
+  sort,
+}: UseExpenseCalculationsProps) {
+  const filteredExpenses = expenses.filter((expense) => {
+    const matchesCategory =
+      !selectedCategory || expense.category === selectedCategory;
+
+    const matchesSearch =
+      !searchTerm ||
+      expense.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+    const matchesTag =
+      !tagSearch ||
+      expense.tags.some((tag) =>
+        tagSearch
+          .split(" ")
+          .every((word) => tag.toLowerCase().includes(word.toLowerCase()))
+      );
+
+    return matchesCategory && matchesSearch && matchesTag;
+  });
+
+  const totalAmount = filteredExpenses.reduce(
+    (acc, expense) => acc + (expense.amount || 0),
+    0
+  );
+
+  const filteredExpensesCount = filteredExpenses.length;
+
+  const amountByCategory = filteredExpenses.reduce((acc, expense) => {
+    const category = expense.category || "No tiene categoria";
+
+    acc[category] = (acc[category] || 0) + (expense.amount || 0);
+
+    return acc;
+  }, {} as Record<string, number>);
+
+  const countByCategory = filteredExpenses.reduce((acc, expense) => {
+    const category = expense.category || "Categoria no existe";
+    acc[category] = (acc[category] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  console.log(amountByCategory);
+  const sortedExpenses = [...filteredExpenses];
+
+  const uniqueTags = [...new Set(expenses.flatMap((expense) => expense.tags))];
+
+  if (sort === "Monto mayor") {
+    sortedExpenses.sort((a, b) => (b.amount || 0) - (a.amount || 0));
+  }
+
+  if (sort === "Monto menor") {
+    sortedExpenses.sort((a, b) => (a.amount || 0) - (b.amount || 0));
+  }
+
+  if (sort === "Mas recientes") {
+    sortedExpenses.sort(
+      (a, b) =>
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+    );
+  }
+
+  if (sort === "Mas antiguos") {
+    sortedExpenses.sort(
+      (a, b) =>
+        new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+    );
+  }
+
+  if (sort === "A-Z") {
+    sortedExpenses.sort((a, b) => a.title.localeCompare(b.title));
+  }
+
+  if (sort === "Z-A") {
+    sortedExpenses.sort((a, b) => b.title.localeCompare(a.title));
+  }
+
+  return {
+    filteredExpensesCount,
+    totalAmount,
+    countByCategory,
+    uniqueTags,
+    sortedExpenses,
+    amountByCategory,
+  };
+}
